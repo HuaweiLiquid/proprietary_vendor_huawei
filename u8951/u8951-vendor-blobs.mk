48c44d76845075ba5c056deb2bffad05a339599d
Y300-0100 <bosko.brajovich@gmail.com>
2014-11-22 17:39:00 +0100

    Initial commit

---
+++ u8951/u8951-vendor-blobs.mk
@@ -0,0 +1,44 @@
+#
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_COPY_FILES += \
+    vendor/huawei/u8951/proprietary/app/SmartcardService.apk:system/app/SmartcardService.apk \
+    vendor/huawei/u8951/proprietary/bin/nfc_check:system/bin/nfc_check \
+    vendor/huawei/u8951/proprietary/etc/EEDATA.cfg:system/etc/EEDATA.cfg \
+    vendor/huawei/u8951/proprietary/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    vendor/huawei/u8951/proprietary/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
+    vendor/huawei/u8951/proprietary/etc/reader.conf:system/etc/reader.conf \
+    vendor/huawei/u8951/proprietary/etc/permissions/org.simalliance.openmobileapi.xml:system/etc/permissions/org.simalliance.openmobileapi.xml \
+    vendor/huawei/u8951/proprietary/framework/org.simalliance.openmobileapi.jar:system/framework/org.simalliance.openmobileapi.jar \
+    vendor/huawei/u8951/proprietary/lib/hw/nfc.huawei.so:system/lib/hw/nfc.huawei.so \
+    vendor/huawei/u8951/proprietary/lib/libifdsmartcardapi.so:system/lib/libifdsmartcardapi.so \
+    vendor/huawei/u8951/proprietary/lib/libassd.so:system/lib/libassd.so \
+    vendor/huawei/u8951/proprietary/MTK_FIRMWARE/mtk_update:system/MTK_FIRMWARE/mtk_update \
+    vendor/huawei/u8951/proprietary/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
+    vendor/huawei/u8951/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
+    vendor/huawei/u8951/proprietary/etc/firmware/BCM4329_check.hcd:system/etc/firmware/BCM4329_check.hcd \
+    vendor/huawei/u8951/proprietary/etc/firmware/BCM4330_check.hcd:system/etc/firmware/BCM4330_check.hcd \
+    vendor/huawei/u8951/proprietary/lib/libpn544_fw.so:system/lib/libpn544_fw.so \
+    vendor/huawei/u8951/proprietary/wifi/udp_server:system/wifi/udp_server \
+    vendor/huawei/u8951/proprietary/wifi/nvram_4330_MSM7225A_H867G.txt:system/wifi/nvram_4330_MSM7225A_H867G.txt \
+    vendor/huawei/u8951/proprietary/wifi/nvram_4330_MSM8X25_C8950D.txt:system/wifi/nvram_4330_MSM8X25_C8950D.txt \
+    vendor/huawei/u8951/proprietary/wifi/nvram_4330.txt:system/wifi/nvram_4330.txt \
+    vendor/huawei/u8951/proprietary/wifi/iwpriv:system/wifi/iwpriv \
+    vendor/huawei/u8951/proprietary/wifi/iwlist:system/wifi/iwlist \
+    vendor/huawei/u8951/proprietary/wifi/iwconfig:system/wifi/iwconfig \
+    vendor/huawei/u8951/proprietary/wifi/fw_4330_b2_test.bin:system/wifi/fw_4330_b2_test.bin \
+    vendor/huawei/u8951/proprietary/wifi/fw_4330_b2_p2p_ap.bin:system/wifi/fw_4330_b2_p2p_ap.bin \
+    vendor/huawei/u8951/proprietary/wifi/fw_4330_b2.bin:system/wifi/fw_4330_b2.bin
